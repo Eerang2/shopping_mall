518074cb02be60fc2a55774da57d3a71a2c65e3b
Eerang2 <dlwlsdn8022@gmail.com>
2025-03-20 15:02:31 +0900

    스케쥴 어노테이션 활성화 시키기위한 세팅 어노테이션

--- src/main/java/shopping_mall/ShoppingMallApplication.java
+++ src/main/java/shopping_mall/ShoppingMallApplication.java
@@ -2,8 +2,10 @@ package shopping_mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ShoppingMallApplication {
 
 	public static void main(String[] args) {
